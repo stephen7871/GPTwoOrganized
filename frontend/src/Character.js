9254346f4570d61b64ac6d333915c268e493850e
Alice Smith <alice@smith.com>
2023-07-06 09:30:39 -0400

    films added

--- frontend/src/Character.js
+++ frontend/src/Character.js
@@ -6,7 +6,6 @@ function Character(){
 
     const navigate = useNavigate();
     const params = useParams();
-    let url = "/localhost:5000";
     async function getCharacter() {
         let fetchedCharacter = await fetch(`http://localhost:5000/characters/${params.id}`);
         fetchedCharacter.homeworld =  await fetch(`http://localhost:5000/planets/${fetchedCharacter.homeworld}`)
@@ -20,7 +19,7 @@ function Character(){
         navigate(`/films/${id}`);
     } 
 
-    
+
     function navtoPlanet(id){
         navigate(`/planets/${id}`);
     }
--- frontend/src/Films.js
+++ frontend/src/Films.js
@@ -6,39 +6,25 @@ function Films(){
 
     let navigate = useNavigate();
     let params = useParams();
-    let url = "/localhost:5000";
-    async function getFilm() {
+
+    useEffect(async () =>  {
         let fetchedFilm = await fetch(`http://localhost:5000/films/${params.id}`);
         fetchedFilm.planets = await fetch(`http://localhost:5000/films/${params.id}/planets`);
         fetchedFilm.characters = await fetch (`http://localhost:5000/films/${params.id}/characters`)
         console.log(fetchedFilm);
         setFilm(fetchedFilm);
-    }
-    
-    async function fetchFilm() {
-        let result = await fetch(`${url}/films/${params.id}`);
-        return result.json();
-    }
+    },  []);
 
-    const fetchCharacters = async () => {
-        let ret = await fetch (`${url}/films/${params.id}/characters`)
-        .then((res) => res.json());
-        return ret;
-    };
-
-    const fetchPlanets = async () => {
-        let ret = await fetch(`${url}/films/${params.id}/planets`).then((res) => res.json());
-        return ret;
-    };
-
-    useEffect(() => getFilm, []);
-
-    function handlePlanetClick(id){
+    function goToPlanet(id){
         navigate(`/planets/${id}`);
     }
 
-    function handleCharacterClick(id){
+    function goToCharacter(id){
         navigate(`/characters/${id}`);
     }
 
-}
+
+    return(
+          <h1>film page</h1>);
+    
+          }
